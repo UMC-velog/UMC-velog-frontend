38853140dc54e97f6a0cd488cf602bfb19ee35b6
seorinn <seorin@ajou.ac.kr>
2023-11-08 22:21:49 +0900

    React app 세팅

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import {  Routes, Route } from 'react-router-dom';
+// import axios from 'axios';
+
+import Main from './pages/Main.js';
+import Post from './pages/Post.js';
+import Profile from './pages/Profile.js';
+import Search from './pages/Search.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path = "/" element={<Main/>}/>
+        <Route path = "/post" element={<Post/>}/>
+        <Route path = "/profile" element={<Profile/>}/>
+        <Route path = "/search" element={<Search/>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
