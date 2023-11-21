4a8e79be13d5f33630077903b57b809d911f512b
seorinn <seorin@ajou.ac.kr>
2023-11-22 01:50:10 +0900

    Feat: LoginHandler 삭제

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import React, { useState } from 'react';
+import React from 'react';
 import {  Routes, Route } from 'react-router-dom';
 
 import Main from './pages/Main.js';
@@ -10,31 +10,18 @@ import Search from './pages/Search.js';
 import SignUp from './pages/SignUp.js';
 import Write from './pages/Write.js';
 
-export const LoginStateContext = React.createContext();
-
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  
-  const LoginHandler = (setLogin) => {
-    if(setLogin !== null){
-      setIsLoggedIn(setLogin);
-    }
-    return isLoggedIn;
-  }
-
   return (
-    <LoginStateContext.Provider value={LoginHandler}>
-      <div className="App">
-        <Routes>
-          <Route path = "/" element={<Main/>}/>
-          <Route path = "/write" element={<Write/>}/>
-          <Route path = "/:userId/:postTitle" element={<Post/>}/>
-          <Route path = "/:userId" element={<Profile/>}/>
-          <Route path = "/search" element={<Search/>}/>
-          <Route path = "/register/:email" element={<SignUp/>}/>
-        </Routes>
-      </div>
-    </LoginStateContext.Provider>
+    <div className="App">
+      <Routes>
+        <Route path = "/" element={<Main/>}/>
+        <Route path = "/write" element={<Write/>}/>
+        <Route path = "/:boardId" element={<Post/>}/>
+        <Route path = "/profile/:userId" element={<Profile/>}/>
+        <Route path = "/search" element={<Search/>}/>
+        <Route path = "/register/:email" element={<SignUp/>}/>
+      </Routes>
+    </div>
   );
 }
 
